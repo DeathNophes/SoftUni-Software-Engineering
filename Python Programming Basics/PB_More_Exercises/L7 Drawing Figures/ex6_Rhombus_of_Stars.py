c71dbcd2bb135fae1f0bf7e02bd9f9abde66c2ca
DeathNophes <145775812+DeathNophes@users.noreply.github.com>
2023-09-22 20:19:58 +0300

    Add files via upload

---
+++ Python Programming Basics/PB_More_Exercises/L7 Drawing Figures/ex6_Rhombus_of_Stars.py
@@ -0,0 +1,19 @@
+n = int(input())
+
+for row in range(1, n + 1):
+    for col in range(1, n - row + 1):
+        print(' ', end='')
+    print('*', end='')
+    #   Този цикъл отпечатва space-овете и първата звездичка
+    for col in range(1, row):
+        print(' *', end='')
+    print()
+    #   Този цикъл отпечатва останалите звездички, и нов ред
+for row1 in range(n - 1, 0, -1):
+    for col1 in range(1, n - row1 + 1):
+        print(' ', end='')
+    print('*', end='')
+
+    for col1 in range(1, row1):
+        print(' *', end='')
+    print()
