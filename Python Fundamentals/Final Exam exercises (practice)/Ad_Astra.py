7ac1b7a9a4ead05804b3ab8e94e719a7b17a5721
DeathNophes <145775812+DeathNophes@users.noreply.github.com>
2023-12-03 09:41:36 +0200

    Update Ad_Astra.py

--- Python Fundamentals/Final Exam exercises (practice)/Ad_Astra.py
+++ Python Fundamentals/Final Exam exercises (practice)/Ad_Astra.py
@@ -3,17 +3,12 @@ import re
 items = input()
 pattern = r'([#|]{1})([\sA-Za-z]+)\1(\d{2}/\d{2}/\d{2})\1(\d+)\1'
 calories_per_day = 2000
-
-my_calories = 0
-foods = re.finditer(pattern, items)
-for food in foods:
-    my_calories += int(food.group(4))
-days = my_calories // calories_per_day
+foods = re.findall(pattern, items)
+my_calories = sum(int(food[3]) for food in foods)
+days = my_calories // 2000
 print(f"You have food to last you for: {days} days!")
-
-list_foods = re.finditer(pattern, items)
-for current_food in list_foods:
-    item = current_food.group(2)
-    expiration_date = current_food.group(3)
-    calories = current_food.group(4)
+for food in foods:
+    item = food[1]
+    expiration_date = food[2]
+    calories = food[3]
     print(f"Item: {item}, Best before: {expiration_date}, Nutrition: {calories}")
