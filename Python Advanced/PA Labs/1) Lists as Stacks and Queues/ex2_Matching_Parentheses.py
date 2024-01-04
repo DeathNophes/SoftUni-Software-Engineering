2dd3b6911e1cfa8dee96a7e69783d8cc073912e3
DeathNophes <145775812+DeathNophes@users.noreply.github.com>
2024-01-04 15:01:44 +0200

    Create ex2_Matching_Parentheses.py

---
+++ Python Advanced/PA Labs/1) Lists as Stacks and Queues/ex2_Matching_Parentheses.py
@@ -0,0 +1,8 @@
+expression = input()
+stack = []
+for index in range(len(expression)):
+    if expression[index] == '(':
+        stack.append(index)
+    elif expression[index] == ')':
+        start = stack.pop()
+        print(expression[start:index + 1])
