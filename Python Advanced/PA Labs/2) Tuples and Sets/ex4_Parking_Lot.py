5d48dc8a1a678b3d8344558f80179e0a0f1cbce9
DeathNophes <145775812+DeathNophes@users.noreply.github.com>
2024-01-10 22:34:59 +0200

    Create ex4_Parking_Lot.py

---
+++ Python Advanced/PA Labs/2) Tuples and Sets/ex4_Parking_Lot.py
@@ -0,0 +1,14 @@
+n = int(input())
+cars = set()
+for _ in range(n):
+    direction, car_number = input().split(', ')
+    if direction == 'IN':
+        cars.add(car_number)
+    elif direction == 'OUT' and car_number in cars:
+        cars.remove(car_number)
+
+if cars:
+    for car in cars:
+        print(car)
+else:
+    print("Parking Lot is Empty")
