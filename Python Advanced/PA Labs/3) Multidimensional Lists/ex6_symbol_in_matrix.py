f0e2f462c0cc6c6cd84acae3c1644f815a9232b4
DeathNophes <145775812+DeathNophes@users.noreply.github.com>
2024-01-18 00:08:42 +0200

    Update

--- Python Advanced/PA Labs/3) Multidimensional Lists/ex6_symbol_in_matrix.py
+++ Python Advanced/PA Labs/3) Multidimensional Lists/ex6_symbol_in_matrix.py
@@ -6,7 +6,8 @@ for row_index in range(len(matrix)):
     for col_index in range(len(matrix[row_index])):
         curr_element = matrix[row_index][col_index]
         if curr_element == char:
-            print(f"({row_index}, {matrix[row_index].index(char)})")
+            char_index = matrix[row_index].index(char)
+            print(f"({row_index}, {char_index})")
             exit()
 
 print(f"{char} does not occur in the matrix")
