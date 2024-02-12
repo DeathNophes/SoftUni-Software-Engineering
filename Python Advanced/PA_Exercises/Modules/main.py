7fa6ab2aa21f0894475a984421011088ae4bb0f3
DeathNophes <145775812+DeathNophes@users.noreply.github.com>
2024-02-12 20:32:04 +0200

    Create main.py

---
+++ Python Advanced/PA_Exercises/Modules/main.py
@@ -0,0 +1,6 @@
+from authentication import render_entry
+from canvas import root
+
+if __name__ == "__main__":  # This is the place where we start the program
+    render_entry()
+    root.mainloop()
