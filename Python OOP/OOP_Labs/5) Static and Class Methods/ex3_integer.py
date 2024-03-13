cff0403aa99075b8dcc0ae1160f66a6badcb7aea
DeathNophes <145775812+DeathNophes@users.noreply.github.com>
2024-03-13 11:37:25 +0200

    Create ex3_integer.py

---
+++ Python OOP/OOP_Labs/5) Static and Class Methods/ex3_integer.py
@@ -0,0 +1,31 @@
+class Integer:
+    ROMAN = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+
+    def __init__(self, value: int):
+        self.value = value
+
+    @classmethod
+    def from_float(cls, value):
+        if not isinstance(value, float):
+            return "value is not a float"
+
+        return cls(int(value))
+
+    @classmethod
+    def from_roman(cls, value):
+        result = 0
+
+        for i in range(len(value)):
+            if i != 0 and cls.ROMAN[value[i]] > cls.ROMAN[value[i - 1]]:
+                result += cls.ROMAN[value[i]] - 2 * cls.ROMAN[value[i - 1]]
+            else:
+                result += cls.ROMAN[value[i]]
+
+        return cls(result)
+
+    @classmethod
+    def from_string(cls, value):
+        if not isinstance(value, str):
+            return "wrong type"
+
+        return cls(int(value))
