724819774dcffa13acac087edbf6ba8169cfc846
DeathNophes <145775812+DeathNophes@users.noreply.github.com>
2024-03-22 16:47:31 +0200

    Create mammals.py

---
+++ Python OOP/OOP_Exercises/6) Polymorphism and Abstraction - Exercises/ex4_wild_farm/animals/mammals.py
@@ -0,0 +1,62 @@
+from ex4_wild_farm.animals.animal import Mammal
+from ex4_wild_farm.food import Fruit, Vegetable, Meat
+
+
+class Mouse(Mammal):
+
+    @property
+    def food_that_eats(self) -> list:
+        return [Fruit, Vegetable]
+
+    @property
+    def gained_weight(self) -> float:
+        return 0.10
+
+    @staticmethod
+    def make_sound() -> str:
+        return "Squeak"
+
+
+class Dog(Mammal):
+
+    @property
+    def food_that_eats(self) -> list:
+        return [Meat]
+
+    @property
+    def gained_weight(self) -> float:
+        return 0.40
+
+    @staticmethod
+    def make_sound() -> str:
+        return "Woof!"
+
+
+class Cat(Mammal):
+
+    @property
+    def food_that_eats(self) -> list:
+        return [Meat, Vegetable]
+
+    @property
+    def gained_weight(self) -> float:
+        return 0.30
+
+    @staticmethod
+    def make_sound() -> str:
+        return "Meow"
+
+
+class Tiger(Mammal):
+
+    @property
+    def food_that_eats(self) -> list:
+        return [Meat]
+
+    @property
+    def gained_weight(self) -> float:
+        return 1.00
+
+    @staticmethod
+    def make_sound() -> str:
+        return "ROAR!!!"
