c0edfab38e0b94ea8b9cd786570012e6be3d7224
DeathNophes <145775812+DeathNophes@users.noreply.github.com>
2024-04-03 14:45:55 +0300

    Create ex1_logged.py

---
+++ Python OOP/OOP_Exercises/9) Decorators - Exercises/ex1_logged.py
@@ -0,0 +1,6 @@
+def logged(func):
+    def wrapper(*args, **kwargs):
+        return f"you called {func.__name__}{args}\n" + \
+               f"it returned {func(*args, **kwargs)}"
+
+    return wrapper
