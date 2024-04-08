c7605bdb5c82e66b6b73433b7adfa7a8773fad64
DeathNophes <145775812+DeathNophes@users.noreply.github.com>
2024-04-08 03:46:29 +0300

    Create robots_managing_app.py

---
+++ Python OOP/OOP_exam_practice/08-Apr-2023/project/robots_managing_app.py
@@ -0,0 +1,95 @@
+from project.robots.male_robot import MaleRobot
+from project.robots.female_robot import FemaleRobot
+from project.services.main_service import MainService
+from project.services.secondary_service import SecondaryService
+
+
+class RobotsManagingApp:
+    VALID_SERVICES = {
+        "MainService": MainService,
+        "SecondaryService": SecondaryService
+    }
+
+    VALID_ROBOTS = {
+        "MaleRobot": MaleRobot,
+        "FemaleRobot": FemaleRobot
+    }
+
+    def __init__(self):
+        self.robots = []
+        self.services = []
+
+    def add_service(self, service_type: str, name: str):
+        try:
+            service = self.VALID_SERVICES[service_type](name)
+        except KeyError:
+            raise Exception("Invalid service type!")
+
+        self.services.append(service)
+        return f"{service_type} is successfully added."
+
+    def add_robot(self, robot_type: str, name: str, kind: str, price: float):
+        try:
+            robot = self.VALID_ROBOTS[robot_type](name, kind, price)
+        except KeyError:
+            raise Exception("Invalid robot type!")
+
+        self.robots.append(robot)
+        return f"{robot_type} is successfully added."
+
+    def add_robot_to_service(self, robot_name: str, service_name: str):
+        robot = [r for r in self.robots if r.name == robot_name][0]
+        service = [s for s in self.services if s.name == service_name][0]
+
+        robot_type = robot.__class__.__name__
+        service_type = service.__class__.__name__
+
+        if (robot_type == 'MaleRobot' and service_type == 'SecondaryService') or \
+                (robot_type == 'FemaleRobot' and service_type == 'MainService'):
+            return "Unsuitable service."
+
+        if service.capacity == len(service.robots):
+            raise Exception("Not enough capacity for this robot!")
+
+        self.robots.remove(robot)
+        service.robots.append(robot)
+
+        return f"Successfully added {robot_name} to {service_name}."
+
+    def remove_robot_from_service(self, robot_name: str, service_name: str):
+        service = [s for s in self.services if s.name == service_name][0]
+
+        try:
+            robot = [r for r in service.robots if r.name == robot_name][0]
+        except IndexError:
+            raise Exception("No such robot in this service!")
+
+        service.robots.remove(robot)
+        self.robots.append(robot)
+
+        return f"Successfully removed {robot_name} from {service_name}."
+
+    def feed_all_robots_from_service(self, service_name: str):
+        service = [s for s in self.services if s.name == service_name][0]
+
+        for robot in service.robots:
+            robot.eating()
+
+        return f"Robots fed: {len(service.robots)}."
+
+    def service_price(self, service_name: str):
+        service = [s for s in self.services if s.name == service_name][0]
+
+        total_price = 0
+        for robot in service.robots:
+            total_price += robot.price
+
+        return f"The value of service {service_name} is {total_price:.2f}."
+
+    def __str__(self):
+        result = ""
+
+        for service in self.services:
+            result += f"{service.details()}\n"
+
+        return result[:-1]
