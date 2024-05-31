7ab7eaa59dc9a0335a5215b3398adf53876c801b
DeathNophes <145775812+DeathNophes@users.noreply.github.com>
2024-06-01 00:06:05 +0300

    Create ex17_monasteries_by_continents_and_countries.sql

---
+++ PostgreSQL/Exercises/6) Subqueries and JOINs - Exercises/ex17_monasteries_by_continents_and_countries.sql
@@ -0,0 +1,27 @@
+UPDATE countries
+SET country_name = 'Burma'
+WHERE country_name = 'Myanmar';
+
+INSERT INTO monasteries (monastery_name, country_code)
+	VALUES
+		('Hanga Abbey', (SELECT country_code FROM countries WHERE country_name = 'Tanzania')),
+    	('Myin-Tin-Daik', (SELECT country_code FROM countries WHERE country_name = 'Myanmar'));
+
+SELECT
+	cont.continent_name,
+    c.country_name,
+    COUNT(m.id) AS "monasteries_count"
+FROM
+    continents AS "cont"
+LEFT JOIN
+	countries AS "c" USING (continent_code)
+LEFT JOIN
+	monasteries AS "m" USING (country_code)
+WHERE
+	NOT c.three_rivers
+GROUP BY
+	c.country_name,
+	cont.continent_name
+ORDER BY
+	monasteries_count DESC,
+	c.country_name;
