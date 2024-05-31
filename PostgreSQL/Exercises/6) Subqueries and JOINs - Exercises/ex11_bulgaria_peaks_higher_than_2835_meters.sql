a9017ab726dead2088cfc31d29b8ee5eca429f28
DeathNophes <145775812+DeathNophes@users.noreply.github.com>
2024-05-31 12:28:34 +0300

    Create ex11_bulgaria_peaks_higher_than_2835_meters.sql

---
+++ PostgreSQL/Exercises/6) Subqueries and JOINs - Exercises/ex11_bulgaria_peaks_higher_than_2835_meters.sql
@@ -0,0 +1,17 @@
+SELECT
+	mc.country_code,
+	m.mountain_range,
+	p.peak_name,
+	p.elevation
+FROM
+	mountains_countries AS "mc"
+JOIN
+	mountains AS "m" ON m.id = mc.mountain_id
+JOIN
+	peaks AS "p" ON mc.mountain_id = p.mountain_id
+WHERE
+	mc.country_code = 'BG'
+		AND
+	p.elevation > 2835
+ORDER BY
+	p.elevation DESC;
