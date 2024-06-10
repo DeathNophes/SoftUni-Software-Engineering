3ed6515e238acb502a361720e01631b031b718da
DeathNophes <145775812+DeathNophes@users.noreply.github.com>
2024-06-10 11:19:53 +0300

    Create ex10_animals_for_adoption.sql

---
+++ PostgreSQL/Exam_practice/Exam Preperation 1/ex10_animals_for_adoption.sql
@@ -0,0 +1,16 @@
+SELECT
+	a.name,
+	TO_CHAR(a.birthdate, 'YYYY') AS "birth_year",
+	at.animal_type
+FROM
+	animals AS "a"
+JOIN
+	animal_types AS "at"
+ON
+	a.animal_type_id = at.id
+WHERE
+	AGE('01/01/2022', a.birthdate) < '5 years'
+	AND at.animal_type <> 'Birds'
+	AND a.owner_id IS NULL
+ORDER BY
+	a.name;
