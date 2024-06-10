e448efd596044cdc087216280da274087d0f8e49
DeathNophes <145775812+DeathNophes@users.noreply.github.com>
2024-06-10 11:19:41 +0300

    Create ex9_volunteers_in_sofia.sql

---
+++ PostgreSQL/Exam_practice/Exam Preperation 1/ex9_volunteers_in_sofia.sql
@@ -0,0 +1,15 @@
+SELECT
+	v.name AS "volunteers",
+	v.phone_number,
+	TRIM(v.address, 'Sofia,  ') AS "address"
+FROM
+	volunteers AS "v"
+JOIN
+	volunteers_departments AS "vd"
+ON
+	v.department_id = vd.id
+WHERE
+	v.address LIKE '%Sofia%'
+	AND vd.department_name = 'Education program assistant'
+ORDER BY
+	volunteers;
