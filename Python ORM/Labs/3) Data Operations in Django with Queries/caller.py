e0e3003ed7040ce04cb87f2e0bc279ffe3ea1dc4
DeathNophes <145775812+DeathNophes@users.noreply.github.com>
2024-06-26 01:08:47 +0300

    Create caller.py

---
+++ Python ORM/Labs/3) Data Operations in Django with Queries/caller.py
@@ -0,0 +1,70 @@
+import os
+import django
+from datetime import date
+
+# Set up Django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
+django.setup()
+
+# Import your models here
+from main_app.models import Student
+
+
+# Run and print your queries
+def add_students():
+    student1 = Student(
+        student_id='FC5204',
+        first_name='John',
+        last_name='Doe',
+        birth_date='1995-05-15',
+        email='john.doe@university.com'
+    )
+    student1.save()
+
+    student2 = Student(
+        student_id='FE0054',
+        first_name='Jane',
+        last_name='Smith',
+        email='jane.smith@university.com'
+    )
+    student2.save()
+
+    Student.objects.create(
+        student_id='FH2014',
+        first_name='Alice',
+        last_name='Johnson',
+        birth_date='1998-02-10',
+        email='alice.johnson@university.com'
+    )
+
+    Student.objects.create(
+        student_id='FH2015',
+        first_name='Bob',
+        last_name='Wilson',
+        birth_date='1996-11-25',
+        email='bob.wilson@university.com'
+    )
+
+
+def get_students_info():
+    students = Student.objects.all()
+
+    result = ""
+    for s in students:
+        result += (f"Student №{s.student_id}: "
+                   f"{s.first_name} "
+                   f"{s.last_name}; "
+                   f"Email: {s.email}\n")
+
+    return result
+
+
+def update_students_emails():
+    students = Student.objects.all()
+    for s in students:
+        s.email = s.email.replace(s.email.split('@')[1], 'uni-students.com')
+        s.save()
+
+
+def truncate_students():
+    students = Student.objects.all().delete()
